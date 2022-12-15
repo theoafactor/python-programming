865cbd9d297e59ad3cef08f0b674af107495e577
Olu <theoafactor@gmail.com>
2022-12-15 14:00:39 +0100

    Added loops

--- loops/project3_while.py
+++ loops/project3_while.py
@@ -63,14 +63,32 @@ print( """
         
     """)
 
-
-
-while True:
+no_of_failed_attempt = 0 ## add a counter for checking the number of failed attempts
+checker = True
+while checker:
     # ask the user for their email
     userEmail = input("Please enter your email: ")
+    print("Great! The email you entered is " + userEmail)
 
-    if userEmail == "james@email.com":
-        print('Welcome User')
-        break
-    else:
-        print("Invalid email .. please try again")
+    ## collect password from the user
+    userPassword = input("Please enter your password: ")
+
+    ## loop through the data
+    for user in users:
+        if user['email'] == userEmail and user['password'] == userPassword:
+            print("Welcome " + user['firstname'] + " " + user['lastname'])
+
+            print("""---> Awards Received: """ + str(user['awards']) + """
+---> Street Name: """ + user["address"]["street_name"] +
+            
+                """ """)
+            checker = False
+            break   
+    else: 
+        no_of_failed_attempt = no_of_failed_attempt + 1
+        if no_of_failed_attempt < 3:
+            print("Invalid user email and password. Please check and try again.") 
+        else:
+            print("You ran out of chances. No more attempt")
+            checker = False
+            break
